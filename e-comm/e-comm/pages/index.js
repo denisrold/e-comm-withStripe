1dc3974c86cbed5f6ed01bca5228d38fcfa809a2
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-01 11:43:17 -0300

    products compoent

--- e-comm/e-comm/pages/index.js
+++ e-comm/e-comm/pages/index.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [productInfo, setProductInfo] = useState([]);
+  useEffect(() => {
+    fetch("/api/products")
+      .then((response) => response.json())
+      .then((json) => setProductInfo(json));
+  }, []);
+
+  const categoryNames = [...new Set(productInfo.map((p) => p.category))];
+
   return (
     <div className="p-5">
       <div>
-        <h2 className="text-2xl">Mobiles</h2>
+        {categoryNames?.map((c) => {
+          return (
+            <div key={c}>
+              <h2 className="text-xl capitalize">{c}</h2>
+              {productInfo
+                .filter((p) => p.category === c)
+                .map((product) => (
+                  <div key={product.name}>{product.name}</div>
+                ))}
+            </div>
+          );
+        })}
         <div className="py-4">
           <div className="w-64">
             <div className="bg-blue-100 rounded-xl p-5">
