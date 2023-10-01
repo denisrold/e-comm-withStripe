d6f343e41d90d34a3579872e3a7ea5a4d8ed7538
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-01 18:29:13 -0300

    SearchBar

--- e-comm/e-comm/pages/index.js
+++ e-comm/e-comm/pages/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [productInfo, setProductInfo] = useState([]);
+  const [phrase, setPhrase] = useState("");
   useEffect(() => {
     fetch("/api/products")
       .then((response) => response.json())
@@ -13,16 +14,29 @@ export default function Home() {
 
   return (
     <div className="p-5">
+      <input
+        value={phrase}
+        onChange={(e) => {
+          setPhrase(e.target.value);
+        }}
+        type="text"
+        placeholder="Search for products..."
+        className="bg-gray-100 w-full py-2 px-4 rounded-xl"
+      />
       <div>
         {categoryNames?.map((c) => {
           return (
             <div key={c}>
-              <h2 className="text-xl capitalize">{c}</h2>
-              {productInfo
-                .filter((p) => p.category === c)
-                .map((productInfo) => (
-                  <Product {...productInfo} />
-                ))}
+              <h2 className="text-xl py-2 capitalize">{c}</h2>
+              <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+                {productInfo
+                  .filter((p) => p.category === c)
+                  .map((productInfo) => (
+                    <div key={productInfo._id} className="px-5 snap-start">
+                      <Product {...productInfo} />
+                    </div>
+                  ))}
+              </div>
             </div>
           );
         })}
