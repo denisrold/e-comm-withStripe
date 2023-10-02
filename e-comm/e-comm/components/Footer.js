d8bebcf8aedde2720283fd76e300b255bce03003
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-02 19:08:40 -0300

    ReactContext ok

--- e-comm/e-comm/components/Footer.js
+++ e-comm/e-comm/components/Footer.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useContext } from "react";
+import { ProductsContext } from "@/components/ProductContext";
 
 export default function Footer() {
   const router = useRouter();
   const path = router.pathname;
+  const { selectedProducts } = useContext(ProductsContext);
+
   return (
     <footer className="sticky bottom-0 bg-white p-5  w-full flex border-t border-gray-200 justify-center space-x-12 text-gray-400">
       <Link
@@ -50,7 +54,7 @@ export default function Footer() {
             d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
           />
         </svg>
-        <span>Cart 0</span>
+        <span>Cart {selectedProducts.length}</span>
       </Link>
     </footer>
   );
--- e-comm/e-comm/components/Product.js
+++ e-comm/e-comm/components/Product.js
@@ -1,4 +1,13 @@
-export default function Product({ name, price, description, picture }) {
+import { useContext } from "react";
+import { ProductsContext } from "./ProductContext";
+
+export default function Product({ _id, name, price, description, picture }) {
+  const { setSelectedProducts } = useContext(ProductsContext);
+
+  function addProducts() {
+    setSelectedProducts((prev) => [...prev, _id]);
+  }
+
   return (
     <div className="w-64">
       <div className="bg-blue-100 rounded-xl p-5">
@@ -10,7 +19,10 @@ export default function Product({ name, price, description, picture }) {
       <p className="text-sm mt-1 leading-4">{description}</p>
       <div className="flex mt-1">
         <div className="text-2xl font-bold grow">${price}</div>
-        <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">
+        <button
+          onClick={addProducts}
+          className="bg-emerald-400 text-white py-1 px-3 rounded-xl"
+        >
           +
         </button>
       </div>
