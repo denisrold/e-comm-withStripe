6b32c032d52eaed31e8244213ba740beb5f5e1f4
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-02 21:30:48 -0300

    shoping cart! ok

--- e-comm/e-comm/pages/checkout.js
+++ e-comm/e-comm/pages/checkout.js
@@ -3,7 +3,7 @@ import { ProductsContext } from "@/components/ProductContext";
 import { useContext, useEffect, useState } from "react";
 
 export default function CheckoutOutPage() {
-  const { selectedProducts } = useContext(ProductsContext);
+  const { selectedProducts, setSelectedProducts } = useContext(ProductsContext);
   const [productsInfos, setProductsInfos] = useState([]);
   useEffect(() => {
     const uniqueIds = [...new Set(selectedProducts)];
@@ -16,6 +16,23 @@ export default function CheckoutOutPage() {
         });
     }
   }, [selectedProducts]);
+
+  function moreOfThisProduct(id) {
+    setSelectedProducts((prev) => [...prev, id]);
+  }
+
+  function lessOfThisProduct(id) {
+    const pos = selectedProducts.indexOf(id);
+    //if position not exist = pos ===-1
+    if (pos !== -1) {
+      //
+      setSelectedProducts((prev) => {
+        //filter and return all diferents at pos;
+        return prev.filter((value, index) => index !== pos);
+      });
+    }
+  }
+
   return (
     <div>
       <Layout>
@@ -34,7 +51,10 @@ export default function CheckoutOutPage() {
                 <div className="flex">
                   <div className="grow">${productInfo.price}</div>
                   <div>
-                    <button className="border border-emerald-500 px-2 rounded-lg text-emerald-500">
+                    <button
+                      onClick={() => lessOfThisProduct(productInfo._id)}
+                      className="border border-emerald-500 px-2 rounded-lg text-emerald-500"
+                    >
                       -
                     </button>
                     <span className="px-3">
@@ -43,7 +63,10 @@ export default function CheckoutOutPage() {
                           .length
                       }
                     </span>
-                    <button className="border border-emerald-500 bg-emerald-500 px-2 rounded-lg text-white">
+                    <button
+                      onClick={() => moreOfThisProduct(productInfo._id)}
+                      className="border border-emerald-500 bg-emerald-500 px-2 rounded-lg text-white"
+                    >
                       +
                     </button>
                   </div>
