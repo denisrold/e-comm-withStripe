ff75c5af9f576dc86d24f266c9e71fa4aa07a199
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-03 19:29:05 -0300

    Succes message

--- e-comm/e-comm/components/Layout.js
+++ e-comm/e-comm/components/Layout.js
@@ -1,8 +1,24 @@
+import { useContext, useEffect, useState } from "react";
 import Footer from "./Footer";
+import { ProductsContext } from "./ProductContext";
 
 export default function Layout({ children }) {
+  const { setSelectedProducts } = useContext(ProductsContext);
+  const [success, setSuccess] = useState(false);
+  useEffect(() => {
+    if (window.location.href.includes("success")) {
+      setSelectedProducts([]);
+      setSuccess(true);
+    }
+  }, []);
+
   return (
     <div>
+      {success && (
+        <div className="mb-5 bg-green-400 text-white text-lg p-5 rounded-xl">
+          Thanks for your order!
+        </div>
+      )}
       <div className="p-5">{children}</div>
       <Footer />
     </div>
